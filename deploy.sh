d5412ded532b89d8b93bf3be7166036a6dc2b5ef
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-08-04 03:04:03 +0100

    optimised deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-# https://docs.docker.com/engine/install/ubuntu/
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-# Install docker packages
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Add user to docker group
- sudo usermod -aG docker $USER
-
  # Pull latest docker image of repo
  docker pull $DOCKERHUB_USERNAME/simplecrud:latest
 
@@ -28,4 +7,4 @@ sudo apt-get update
  docker stop simplecrud
 
  # Run docker container
- docker run -d -p 8080:8080 simplecrud
+ docker run -d -p 8080:8080 --name simplecrud $DOCKERHUB_USERNAME/simplecrud:latest
